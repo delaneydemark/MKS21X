9f5459ae182f2579139919948320347f1bab3ced
delaneydemark <ddemark@stuy.edu>
2017-11-23 23:30:05 -0500

    all CharSequence methods added in ReverseCharSeq.java and tested in Driver

--- 04ReverseCharSeq/ReverseCharSeq.java
+++ 04ReverseCharSeq/ReverseCharSeq.java
@@ -1,12 +1,41 @@
 public class ReverseCharSeq implements CharSequence{
 
-    private String reversed;
+    private char[] reversed;
 
-    public ReversedCharSeq(String a){
-	reversed = "";
-	for (int i = a.length()-1; i>=0; i++){
-	    reversed+= a.substring(i, i+1);
-	}
+    public ReverseCharSeq(String a){
+		reversed = new char[a.length()];
+		for (int i = a.length()-1, j=0; i>=0; i--, j++){
+	  	  reversed[j]= a.charAt(i);
+		}
+    }
+    
+    public int length(){
+    	return reversed.length;
     }
    
+   public char charAt(int index){
+   		if (index<0 || index>= reversed.length){
+   			throw new IndexOutOfBoundsException();
+   		}
+   		return reversed[index];
+   }
+   
+   public CharSequence subSequence(int start, int end){
+   		String s ="";
+   		if (start<0 || end> reversed.length || start>end){
+   			throw new IndexOutOfBoundsException();
+   		}
+   		for (int i = start; i< end; i++){
+   			s+= reversed[i];
+   		}
+   		return s;
+   }
+   
+   public String toString(){
+   		String s = "";
+   		for (int i = 0; i<reversed.length; i++){
+   			s+= reversed[i];
+   		}
+   		return s;
+   }
 }
