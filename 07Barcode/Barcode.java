f6c47a8c67b455acbb9d6ab5cfe8bcdf4c9087ca
delaneydemark <ddemark@stuy.edu>
2017-12-01 15:30:20 -0500

    created barcode

---
+++ 07Barcode/Barcode.java
@@ -0,0 +1,10 @@
+public class Barcode{
+
+    private String zip;
+    private String[] codes={"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::", "|:::|","|::|:","|:|::"};
+
+    public Barcode(String z){
+	zip = z;
+    }
+
+}
